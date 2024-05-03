9057bdcfe45511b9d714888bf4aabffd9d378ac4
Joao Oliveira <up202007580@up.pt>
2024-05-03 17:14:11 +0100

    Video transfer between two peers

---
+++ sink.py
@@ -0,0 +1,237 @@
+import argparse
+import asyncio
+import logging
+import math
+import json
+import os
+
+import cv2
+import numpy
+from aiortc import (
+    RTCIceCandidate,
+    RTCPeerConnection,
+    RTCSessionDescription,
+    VideoStreamTrack,
+)
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
+from aiortc.contrib.signaling import BYE, add_signaling_arguments, create_signaling
+from av import VideoFrame
+
+
+from aiortc.sdp import candidate_from_sdp, candidate_to_sdp
+
+
+def object_from_string(message_str):
+    message = json.loads(message_str)
+    if message["type"] in ["answer", "offer"]:
+        return RTCSessionDescription(**message)
+    elif message["type"] == "candidate" and message["candidate"]:
+        candidate = candidate_from_sdp(message["candidate"].split(":", 1)[1])
+        candidate.sdpMid = message["id"]
+        candidate.sdpMLineIndex = message["label"]
+        return candidate
+    elif message["type"] == "bye":
+        return BYE
+
+
+def object_to_string(obj):
+    if isinstance(obj, RTCSessionDescription):
+        message = {"sdp": obj.sdp, "type": obj.type}
+    elif isinstance(obj, RTCIceCandidate):
+        message = {
+            "candidate": "candidate:" + candidate_to_sdp(obj),
+            "id": obj.sdpMid,
+            "label": obj.sdpMLineIndex,
+            "type": "candidate",
+        }
+    else:
+        assert obj is BYE
+        message = {"type": "bye"}
+    return json.dumps(message, sort_keys=True)
+
+
+class TcpSocketSignaling:
+    def __init__(self, host, port):
+        self._host = host
+        self._port = port
+        self._server = None
+        self._reader = None
+        self._writer = None
+
+    async def connect(self):
+        pass
+
+    async def _connect(self, server):
+        if self._writer is not None:
+            return
+
+        if server:
+            connected = asyncio.Event()
+
+            def client_connected(reader, writer):
+                self._reader = reader
+                self._writer = writer
+                connected.set()
+
+            self._server = await asyncio.start_server(
+                client_connected, host=self._host, port=self._port
+            )
+            await connected.wait()
+        else:
+            self._reader, self._writer = await asyncio.open_connection(
+                host=self._host, port=self._port
+            )
+
+    async def close(self):
+        if self._writer is not None:
+            await self.send(BYE)
+            self._writer.close()
+            self._reader = None
+            self._writer = None
+        if self._server is not None:
+            self._server.close()
+            self._server = None
+
+    async def receive(self):
+        await self._connect(False)
+        try:
+            data = await self._reader.readuntil()
+        except asyncio.IncompleteReadError:
+            return
+        return object_from_string(data.decode("utf8"))
+
+    async def send(self, descr):
+        await self._connect(True)
+        data = object_to_string(descr).encode("utf8")
+        self._writer.write(data + b"\n")
+
+
+class FlagVideoStreamTrack(VideoStreamTrack):
+    """
+    A video track that returns an animated flag.
+    """
+
+    def __init__(self):
+        super().__init__()  # don't forget this!
+        self.counter = 0
+        height, width = 480, 640
+
+        # generate flag
+        data_bgr = numpy.hstack(
+            [
+                self._create_rectangle(
+                    width=213, height=480, color=(255, 0, 0)
+                ),  # blue
+                self._create_rectangle(
+                    width=214, height=480, color=(255, 255, 255)
+                ),  # white
+                self._create_rectangle(width=213, height=480, color=(0, 0, 255)),  # red
+            ]
+        )
+
+        # shrink and center it
+        M = numpy.float32([[0.5, 0, width / 4], [0, 0.5, height / 4]])
+        data_bgr = cv2.warpAffine(data_bgr, M, (width, height))
+
+        # compute animation
+        omega = 2 * math.pi / height
+        id_x = numpy.tile(numpy.array(range(width), dtype=numpy.float32), (height, 1))
+        id_y = numpy.tile(
+            numpy.array(range(height), dtype=numpy.float32), (width, 1)
+        ).transpose()
+
+        self.frames = []
+        for k in range(30):
+            phase = 2 * k * math.pi / 30
+            map_x = id_x + 10 * numpy.cos(omega * id_x + phase)
+            map_y = id_y + 10 * numpy.sin(omega * id_x + phase)
+            self.frames.append(
+                VideoFrame.from_ndarray(
+                    cv2.remap(data_bgr, map_x, map_y, cv2.INTER_LINEAR), format="bgr24"
+                )
+            )
+
+    async def recv(self):
+        pts, time_base = await self.next_timestamp()
+
+        frame = self.frames[self.counter % 30]
+        frame.pts = pts
+        frame.time_base = time_base
+        self.counter += 1
+        return frame
+
+    def _create_rectangle(self, width, height, color):
+        data_bgr = numpy.zeros((height, width, 3), numpy.uint8)
+        data_bgr[:, :] = color
+        return data_bgr
+
+
+async def run(pc, player, recorder, signaling, role):
+    def add_tracks():
+        if player and player.audio:
+            pc.addTrack(player.audio)
+
+        if player and player.video:
+            pc.addTrack(player.video)
+        else:
+            pc.addTrack(FlagVideoStreamTrack())
+
+    @pc.on("track")
+    def on_track(track):
+        print("Receiving %s" % track.kind)
+        recorder.addTrack(track)
+
+    # connect signaling
+    await signaling.connect()
+
+    if role == "offer":
+        # send offer
+        add_tracks()
+        await pc.setLocalDescription(await pc.createOffer())
+        await signaling.send(pc.localDescription)
+
+    # consume signaling
+    while True:
+        obj = await signaling.receive()
+
+        if isinstance(obj, RTCSessionDescription):
+            await pc.setRemoteDescription(obj)
+            await recorder.start()
+
+            if obj.type == "offer":
+                # send answer
+                add_tracks()
+                await pc.setLocalDescription(await pc.createAnswer())
+                await signaling.send(pc.localDescription)
+        elif isinstance(obj, RTCIceCandidate):
+            await pc.addIceCandidate(obj)
+        elif obj is BYE:
+            print("Exiting")
+            break
+
+
+if __name__ == "__main__":
+
+    pc = RTCPeerConnection()
+    recorder = MediaRecorder("video.mp4")
+    signaling=TcpSocketSignaling("127.0.0.1", "8080")
+
+    # run event loop
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(
+            run(
+                pc=pc,
+                player=None,
+                recorder=recorder,
+                signaling=signaling,
+                role='answer',
+            )
+        )
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # cleanup
+        loop.run_until_complete(recorder.stop())
+        loop.run_until_complete(signaling.close())
+        loop.run_until_complete(pc.close())
